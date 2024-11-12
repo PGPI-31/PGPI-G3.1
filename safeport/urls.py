5bfed007aef4705e4e19bb92a48fe48bf38f830d
Rafael Cabello <rafcabran@alum.us.es>
2024-11-12 15:49:22 +0100

    Vistas iniciales

--- safeport/urls.py
+++ safeport/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include
+
+from safeport.views import home
+
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
+    path('auth/', include('authentication.urls')),
 ]
