7f5205e472c8b5a8b51d68e534e1cd1672a10e1b
fracalrod3 <fracalrod3@alum.us.es>
2024-11-21 12:10:05 +0100

    feat: form update

--- cart/forms.py
+++ cart/forms.py
@@ -6,24 +6,24 @@ class CartInstanceForm(forms.ModelForm):
 
     class Meta:
         model = Cart
-        fields = ['model', 'name', 'port', 'available', 'price_per_day']
+        fields = ['created_at', 'updated_at', 'start_date', 'end_date', 'user_id']
         labels = {
-            'model': 'Modelo',
-            'name': 'Nombre',
-            'port': 'Puerto',
-            'available': 'Disponible',
-            'price_per_day': 'Precio por día',
+            'created_at': 'Creación',
+            'updated_at': 'Actualización',
+            'start_date': 'Comienzo',
+            'end_date': 'Finalización',
+            'user_id': 'Cliente',
         }
 
 class CartItemInstanceForm(forms.ModelForm):
 
     class Meta:
         model = CartItem
-        fields = ['model', 'name', 'port', 'available', 'price_per_day']
+        fields = ['number_of_days', 'price_per_day', 'total_price', 'boat_instance_id', 'cart_id']
         labels = {
-            'model': 'Modelo',
-            'name': 'Nombre',
-            'port': 'Puerto',
-            'available': 'Disponible',
-            'price_per_day': 'Precio por día',
+            'number_of_days': 'Días de duración',
+            'price_per_day': 'Precio diario',
+            'total_price': 'Precio total',
+            'boat_instance_id': 'Barco',
+            'cart_id': 'Cesta',
         }
