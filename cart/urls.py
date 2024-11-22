de65056c10f3cdba0d1ad23b82ccf45c94ce0736
fracalrod3 <fracalrod3@alum.us.es>
2024-11-22 13:30:04 +0100

    fix: remove

--- cart/urls.py
+++ cart/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('add/<int:boat_id>/', views.add_to_cart, name='adicion_cesta'),
-    path('remove_from_cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('', views.view_cart, name='mostrar_cesta'),
 
