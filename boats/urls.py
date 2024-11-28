a898ba5895a2097844bf73f0495b539105508bf7
mohamedabouri <mohabo1@alum.us.es>
2024-11-28 17:28:30 +0100

    feat: Busqueda y filtrado hecho!

--- boats/urls.py
+++ boats/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('modelos/', views.listar_modelos, name='listar_modelos'),
-    path('catalogo/', views.ver_catalogo, name='ver_catalogo'),
+    path('catalogo/', views.filter_boats, name='filtrar_barcos'),
     path('modelos/create/', views.create_boat_model, name='crear_modelo'),
     path('tipos/', views.listar_tipos, name='listar_tipos'),
     path('tipos/create', views.create_boat_type, name='crear_tipo'),
@@ -12,6 +12,6 @@ urlpatterns = [
     path('puertos/create/', views.create_port, name='crear_puerto'),
     path('', views.listar_productos, name='listar_productos'),
     path('create/', views.create_boat_instance, name='crear_productos'),
-    path('modelos/<int:model_id>/', views.mostrar_modelo, name='mostrar_modelo'),
+    path('modelos/<int:model_id>/', views.mostrar_modelo, name='mostrar_modelo')
 
 ]
