e5a9a05f4f37e286e45982e55429e398c7e6c0f2
Rafael Cabello <rafcabran@alum.us.es>
2024-11-29 19:12:31 +0100

    listado y ver usuarios admin

--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,12 @@
 import logging
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.contrib.auth.signals import user_logged_in
 
 from authentication.forms import UserLoginForm, UserRegisterForm
+from authentication.models import User
 from cart.views import merge_carts
 
 def user_register(request):
@@ -36,3 +37,14 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect('home')
+
+
+# View to list all users
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'admin/user_list.html', {'users': users})
+
+# View to show individual user details
+def user_detail(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    return render(request, 'admin/user_detail.html', {'user': user})
