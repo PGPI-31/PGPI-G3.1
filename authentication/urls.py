88f986d19e939b2f319a9f6c5a64ec26134cf876
Iván Ramírez <ivaramlar@alum.us.es>
2024-12-05 12:51:01 +0100

    feat: Añadida modificacion de user

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('users/', views.user_list, name='user_list'),
-    path('users/<int:user_id>/', views.user_detail, name='user_detail')
+    path('users/<int:user_id>/', views.user_detail, name='user_detail'),
+    path('users/modify/<int:user_id>/', views.modify_user, name='modify_user')
+
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.urls import reverse_lazy
+from django.http import HttpResponseForbidden
 from django.contrib.auth.signals import user_logged_in
-
+from django.contrib.auth.decorators import login_required
 from authentication.forms import UserLoginForm, UserRegisterForm
 from authentication.models import User
 from cart.views import merge_carts
@@ -48,3 +49,19 @@ def user_list(request):
 def user_detail(request, user_id):
     user = get_object_or_404(User, id=user_id)
     return render(request, 'admin/user_detail.html', {'user': user})
+
+@login_required
+def modify_user(request, user_id):
+    if not request.user.is_superuser:
+        return HttpResponseForbidden("No tienes permiso para modificar a los usuarios.")
+    
+    user = get_object_or_404(User, id=user_id)
+
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user_list')
+    else:
+        form = UserRegisterForm(instance=user)
+    return render(request, 'admin/modify_user.html', {'form': form})
